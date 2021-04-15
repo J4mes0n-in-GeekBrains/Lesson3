14470e41af26fef057a4c88540b5c23aa5b03704
Maxim <maksgmq@mail.ru>
2021-04-15 23:38:34 +0500

    Initial commit

---
+++ src/main/java/geekbrains_lesson3/Lesson3.java
@@ -0,0 +1,92 @@
+package geekbrains_lesson3;
+
+import java.util.Arrays;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        invertArray();
+        System.out.println("замена");
+        System.out.println("Задание 2");
+        fillArray();
+        System.out.println("Задание 3");
+        changeArray();
+        System.out.println("");
+        System.out.println("Задание 4");
+        fillDiagonal();
+        System.out.println("Задание 5");
+        flashback();
+
+    }
+
+    public static void invertArray() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i + "-" + arr[i] + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else arr[i] = 1;
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(i + "-" + arr[i] + " ");
+        {
+        }
+    }
+
+    public static void fillArray() {
+        int[] arr = new int[100];
+        arr[0] = 0;
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(i + "-" + arr[i] + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < arr.length; i++) {
+        }
+    }
+
+    public static void changeArray() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) arr[i] = arr[i] * 2;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(i + "-" + arr[i] + " ");
+        }
+    }
+
+    public static void fillDiagonal() {
+        int counter = 1;
+        int[][] table = new int[4][4];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                if (counter == 4) {
+                    counter = 1;
+                }
+                table[i][j] = counter;
+                System.out.print(table[i][j] + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void flashback() {
+        int len;
+        int initialValue;
+        len = 25;
+        initialValue = 3;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++)
+        arr[i] = initialValue;
+       System.out.println(Arrays.toString(arr));
+        }
+
+    }
+
+
+
+
+
+
